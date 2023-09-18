c4064c1fd2c28eaef53e4d9295d5786dae9401b1
Sancer <urielreina@gmail.com>
2023-09-18 23:36:54 +0200

    Big bang

---
+++ tests/examples.py
@@ -0,0 +1,17 @@
+import os # noqa: F401
+
+
+class Hello:
+    def __init__(self):
+        self.hello = "hello"
+
+    def say_hello(self):
+        print(self.hello)
+
+
+class Greeting:
+    def __init__(self):
+        self.greeting = "greeting"
+
+    def say_greeting(self):
+        print(self.greeting)
