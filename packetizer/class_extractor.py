102538b3c27b31639fac11b7d42b8a3e32fa68e9
Pol Lopez <pol.lopez.cano@gmail.com>
2023-10-02 21:06:37 +0200

    feat: now removes unused imports

--- packetizer/class_extractor.py
+++ packetizer/class_extractor.py
@@ -1,4 +1,19 @@
 import ast
+from dataclasses import dataclass
+from typing import Optional
+
+
+@dataclass
+class NameDto:
+    name: str
+    alias: Optional[str]
+
+
+@dataclass
+class ImportDto:
+    module: str
+    alias: Optional[str]
+    names: Optional[list[NameDto]]
 
 
 class ClassExtractor(ast.NodeVisitor):
@@ -6,15 +21,19 @@ class ClassExtractor(ast.NodeVisitor):
         self.classes = {}
         self.imports = []
 
-    def visit_Import(self, node):
+    def visit_Import(self, node: ast.Import):
         for n in node.names:
-            self.imports.append(f"import {n.name}")
+            import_dto = ImportDto(module=n.name, alias=n.asname, names=[])
+            self.imports.append(import_dto)
 
-    def visit_ImportFrom(self, node):
+    def visit_ImportFrom(self, node: ast.ImportFrom):
         module = node.module
-        names = [n.name for n in node.names]
-        self.imports.append(f"from {module} import {', '.join(names)}")
+        node_names = []
+        for n in node.names:
+            node_names.append(NameDto(name=n.name, alias=n.asname))
+        import_dto = ImportDto(module=module, alias=None, names=node_names)
+        self.imports.append(import_dto)
 
-    def visit_ClassDef(self, node):
+    def visit_ClassDef(self, node: ast.ClassDef):
         source_lines = ast.unparse(node).split('\n')
         self.classes[node.name] = "\n".join(source_lines)
